a528cb57dd017dde73d17dff8ec244f8a73fdcb5
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-03-05 08:30:37 +0100

    feat() End tests and code
    Final code changes with tests
    
    S10,L132

--- currency-convert.js
+++ currency-convert.js
@@ -1,2 +1,39 @@
 // USD CAD 23
 // 23 USD is worth 28 CAD. You can spend these in the following countries
+
+const axios = require('axios');
+
+const baseUrlFixer = 'https://api.fixer.io/latest';
+const baseCurrency = 'https://restcountries.eu/rest/v2/currency';
+
+const getExchangeRate = (from, to) =>
+  axios.get(`${baseUrlFixer}?base=${from}`)
+    .then(response => response.data.rates[to])
+    .catch(console.log);
+
+const getCountries = currencyCode =>
+  axios.get(`${baseCurrency}/${currencyCode}`)
+    .then(response => response.data.map(country => country.name));
+
+const convertCurrency = (from, to, amount) => {
+  return getCountries(to)
+    .then(countries => getExchangeRate(from, to)
+      .then(rate => {
+        const exchangedAmount = rate * amount;
+
+        return `${amount} ${from} is worth ${exchangedAmount} ${to}. 
+          ${to} can be used in the following
+          countries ${countries.join(', ')}`;
+      }));
+};
+
+const covertCurrencyAlt = async(from, to, amount) => {
+  const countries = await getCountries(to);
+  const rate = await getExchangeRate(from, to);
+  const exchangedAmount = rate * amount;
+  return `${amount} ${from} is worth ${exchangedAmount} ${to}. 
+          ${to} can be used in the following
+          countries ${countries.join(', ')}`;
+};
+
+module.exports = {getExchangeRate, getCountries, convertCurrency, covertCurrencyAlt};
---
+++ test/currency-convert.test.js
@@ -0,0 +1,33 @@
+const {expect} = require('chai');
+const {getExchangeRate, getCountries, convertCurrency, covertCurrencyAlt} = require('./../currency-convert');
+
+/* global define, it, describe, before, beforeEach, afterEach, after */
+describe('Currency convert', () => {
+  it('should get the currency from USD to CAD', () => {
+    return getExchangeRate('USD', 'CAD').then(rate => expect(rate).to.be.a('number'));
+  });
+  it('should get all the countries with USD', () => {
+    return getCountries('USD')
+      .then(countries => expect(countries).to.be.an('array').and.have.length(20));
+  });
+  it('should get all the countries with EUR', () => {
+    return getCountries('EUR')
+      .then(countries => expect(countries).to.be.an('array').and.have.length(36).to.include('Spain'));
+  });
+  it('should convert currency from USD to CAD', () => {
+    return convertCurrency('USD', 'CAD', 12)
+      .then(response => expect(response).to.include('12 USD is worth 15.456 CAD').and.to.include('Canada'));
+  });
+  it('should convert currency from CAD to USD', () => {
+    return convertCurrency('CAD', 'USD', 100)
+      .then(response => expect(response)
+        .to.include('100 CAD is worth 77.639 USD')
+        .and.to.include('United States of America'));
+  });
+  it.only('should convert currency from CAD to USD with async await', () => {
+    return covertCurrencyAlt('CAD', 'USD', 100)
+      .then(response => expect(response)
+        .to.include('100 CAD is worth 77.639 USD')
+        .and.to.include('United States of America'));
+  }).timeout(4000);
+});
